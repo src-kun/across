7548c66c2e9390ad5a0565dfb59c5b7c8a93cfca
src-kun <9331031@qq.com>
2019-09-26 14:42:41 +0800

    Update bbr.sh

--- bbr.sh
+++ bbr.sh
@@ -168,16 +168,6 @@ arch=$( uname -m )
 lbit=$( getconf LONG_BIT )
 kern=$( uname -r )
 
-get_char() {
-    SAVEDSTTY=`stty -g`
-    stty -echo
-    stty cbreak
-    dd if=/dev/tty bs=1 count=1 2> /dev/null
-    stty -raw
-    stty echo
-    stty $SAVEDSTTY
-}
-
 getversion() {
     if [[ -s /etc/redhat-release ]]; then
         grep -oE  "[0-9.]+" /etc/redhat-release
@@ -369,7 +359,5 @@ echo
 echo " URL: https://teddysun.com/489.html"
 echo "----------------------------------------"
 echo
-echo "Press any key to start...or Press Ctrl+C to cancel"
-char=`get_char`
 
 install_bbr 2>&1 | tee ${cur_dir}/install_bbr.log
